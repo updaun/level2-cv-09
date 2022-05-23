387986ab7547b795ec996677dc41aef5ed1ab5d7
JeongJae Park <jeongjae1996@naver.com>
2022-05-23 20:24:12 +0900

    SW Expert Academy 자릿수 더하기

---
+++ src/jj/algorithm/SW_Expert_Academy/2058_sum_digits_D1.py
@@ -0,0 +1,10 @@
+# source: https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=1&contestProbId=AV5QPRjqA10DFAUq&categoryId=AV5QPRjqA10DFAUq&categoryType=CODE&problemTitle=&orderBy=RECOMMEND_COUNT&selectCodeLang=PYTHON&select-1=1&pageSize=10&pageIndex=1
+
+number = input()
+
+answer = 0
+
+for num in number:
+    answer += int(num)
+
+print(answer)
