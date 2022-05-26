5509ba76462ec0a52216bacb82ae5769201a8477
JJ <jeongjae1996@naver.com>
2022-05-26 15:58:41 +0900

    SW Expert Academy 거꾸로 출력해 보아요

---
+++ src/jj/algorithm/SW_Expert_Academy/1545_reverse_D1.py
@@ -0,0 +1,6 @@
+# source: https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=1&contestProbId=AV2gbY0qAAQBBAS0&categoryId=AV2gbY0qAAQBBAS0&categoryType=CODE&problemTitle=&orderBy=RECOMMEND_COUNT&selectCodeLang=PYTHON&select-1=1&pageSize=10&pageIndex=1
+
+n = int(input())
+
+for i in range(n, -1, -1):
+    print(i, end=' ')
