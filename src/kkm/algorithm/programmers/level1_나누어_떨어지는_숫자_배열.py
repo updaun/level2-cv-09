9c3a047c12b09a3099132a89fdb6ad203587fbb1
Kim Kyumin <rrakim31@korea.ac.kr>
2022-06-20 15:17:55 +0900

    프로그래머스 연습문제 나누어 떨어지는 숫자 배열

---
+++ src/kkm/algorithm/programmers/level1_나누어_떨어지는_숫자_배열.py
@@ -0,0 +1,11 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12910
+
+def solution(arr, divisor):
+    answer = []
+    
+    for num in arr:
+        if num % divisor == 0:
+            answer.append(num)
+    if answer == []:
+        return [-1]
+    return sorted(answer)
