47bbbe78fb5e6fd073db9603ca55f2872a25ff94
JJ <jeongjae1996@naver.com>
2022-06-30 22:59:42 +0900

    HackerRank Algorithms Gradeing Students

---
+++ src/jj/algorithm/HackerRank/grading_students.py
@@ -0,0 +1,46 @@
+# source: https://www.hackerrank.com/challenges/grading/problem?isFullScreen=true
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    answer = []
+    
+    for grade in grades:
+        
+        if grade % 5 >= 3 and grade >= 38:
+            grade = grade + 5 - grade % 5
+        
+        answer.append(grade)
+        
+    return answer
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
