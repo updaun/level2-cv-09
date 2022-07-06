8a7b7dce8c8487164a5f1d48527eec66b650309a
JJ <jeongjae1996@naver.com>
2022-07-06 22:49:04 +0900

    BAEKJOON 23560 약

---
+++ src/jj/algorithm/baekjoon/23560_pill.py
@@ -0,0 +1,23 @@
+# source: https://www.acmicpc.net/problem/23560
+# ref: https://dong-gas.tistory.com/36
+
+'''
+N일 동안 약 먹기
+3N개 일렬로 (아침, 점심, 저녁)
+앞에 있는 것과 뒤에 있는 것만 뜯어서 먹을 수 있다.
+
+아침 약 == 저녁 약
+'''
+
+# dynamic programming
+n = int(input())
+
+a = 2 
+b = 1
+
+for _ in range(n-1):
+    tmp_a = a
+    a = 2*a + 2*b
+    b = b + tmp_a
+
+print(a)
