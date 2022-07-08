77b898433043c9e9a1691ef6859eed89ae749e6e
updaun <updauney@daum.net>
2022-07-09 01:36:27 +0900

    220709 TIL
    백준 2309번 - 일곱 난쟁이 풀이 : 브루트포스 풀이, 조합을 활용한 다른 사람 풀이 공부

--- src/jdu/algorithm/boj/question_2309.py
+++ src/jdu/algorithm/boj/question_2309.py
@@ -2,10 +2,26 @@ import sys
 drawf = []
 for i in range(9):
     drawf.append(int(sys.stdin.readline()))
-target = sorted(drawf)
-temp = 0
-while sum(target[temp:]) < 100:
-    temp += 1
-else:
-    for i in target[temp:temp+8]:
-        print(i)
+drawf = sorted(drawf)
+target = sum(drawf)-100
+for i in drawf:
+    for j in drawf:
+        if i != j:
+            if target == i+j:
+                for t in drawf:
+                    if t != i and t != j:
+                        print(t)
+                exit(0)
+
+# 조합을 활용한 다른 사람 풀이
+# from itertools import combinations
+# drawf = [int(input()) for _ in range(9)]
+# occation = list(combinations(drawf, 7))
+# print(occation)
+# for i in occation:
+#     if sum(i) is 100:
+#         answer = list(i)
+#         break
+# answer.sort()
+# for i in answer:
+#     print(i)
