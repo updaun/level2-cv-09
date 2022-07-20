67ea5e6d9a7b858d732a7b39696ca3e870a53d0f
JJ <jeongjae1996@naver.com>
2022-07-20 19:00:59 +0900

    LeetCode 131 Palindrome Partitioning

---
+++ src/jj/algorithm/leetcode/131_palindrome_partitioning.py
@@ -0,0 +1,30 @@
+# source: https://leetcode.com/problems/palindrome-partitioning/
+# ref: https://www.youtube.com/watch?v=3jvWodd7ht0
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        part = []
+        
+        def dfs(i):
+            if i >= len(s):
+                res.append(part.copy())
+                return
+            
+            for j in range(i, len(s)):
+                if self.isPali(s, i, j):
+                    part.append(s[i:j+1])
+                    dfs(j + 1)
+                    print(part.pop())
+                    
+        dfs(0)
+        
+        return res
+    
+    def isPali(self, s, l, r):
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l, r = l + 1, r - 1
+        
+        return True
