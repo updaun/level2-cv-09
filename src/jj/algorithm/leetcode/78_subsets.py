53ebec0bc591ec644db47f6934d0c5d8bd6eb4ec
JeongJae Park <jeongjae1996@naver.com>
2022-07-28 14:40:44 +0900

    LeetCode 78 Subsets

---
+++ src/jj/algorithm/leetcode/78_subsets.py
@@ -0,0 +1,13 @@
+from itertools import combinations
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        
+        for i in range(len(nums)+1):
+            combination = list(map(list, combinations(nums, i)))
+            
+            for c in combination:
+                ans.append(c)
+            
+        return ans
