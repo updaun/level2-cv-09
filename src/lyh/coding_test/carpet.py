5f49e2e713b03d156c198a6573a69037c41657bb
YOONGHEE <yoonghee@YOONGHEEui-MacBookAir.local>
2022-10-13 12:44:01 +0900

    change code

--- src/lyh/coding_test/carpet.py
+++ src/lyh/coding_test/carpet.py
@@ -4,14 +4,16 @@
 import math
 
 def solution(brown, yellow):
+    answer = []
     total = brown + yellow
     h = int(math.sqrt(total))
     
-    while True:
+    while h > 0:
         if (total / h) % 1 == 0:
             w = total / h
             if w >= h:
                 if 2 * w + 2 * h == 4 + brown:
                     return [w, h]
-        
         h -= 1
+    
+    return answer
